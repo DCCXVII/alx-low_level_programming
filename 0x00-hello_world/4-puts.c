6167390672b1e699d83969a264eb43a6225e9a8f
DCCXVII <elyass.hafdi@gmail.com>
2023-03-16 15:27:59 -0700

    alx-low

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,15 +3,10 @@
 /**
  * main - Entry point
  *
- * Description: Prints the string "Programming is like building a multilingual puzzle"
- *              using the puts function.
- *
  * Return: Always 0 (Success)
  */
-
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
-	return (0);
+  puts("\"Programming is like building a multilingual puzzle");
+  return (0);
 }
-
