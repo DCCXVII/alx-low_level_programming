c14959202da9b8b5ff2d0e62e72b7bc7e7c8a0a1
DCCXVII <elyass.hafdi@gmail.com>
2023-03-23 17:13:41 -0700

    alx-low_programming

---
+++ 0x03-debugging/positive_or_negative.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * positive_or_negative - prints whether a number is positive, negative or zero
+ */
+void positive_or_negative(int n)
+{
+if (n > 0)
+printf("%d is positive\n", n);
+else if (n == 0)
+printf("%d is zero\n", n);
+else
+printf("%d is negative\n", n);
+}
+
