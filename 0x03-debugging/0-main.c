707e178e052295af081b946d10aa4e8d932bf3dc
DCCXVII <elyass.hafdi@gmail.com>
2023-03-23 19:17:51 -0700

    alx-system

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,10 +1,10 @@
 #include "main.h"
 
 /**
- * main - tests function that prints if integer is positive or negative
- * Return: 0
+ * main - Entry point
+ * Description: Tests the function that prints whether an integer is positive or negative
+ * Return: Always 0
  */
-
 int main(void)
 {
 int i;
@@ -12,5 +12,5 @@ int i;
 i = 0;
 positive_or_negative(i);
 
-return (1);
+return (0);
 }
