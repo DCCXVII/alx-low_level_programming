2e340a93a2a7e32aab18fc7f523a9e96e3b40362
DCCXVII <elyass.hafdi@gmail.com>
2023-03-23 17:28:11 -0700

    alx-low_programming

---
+++ 0x03-debugging/2-largest_number.c
@@ -0,0 +1,26 @@
+/**
+ * largest_number 
+ * @a: first integer
+ * @b: second integer
+ * @c: third integer
+ */
+
+int largest_number(int a, int b, int c)
+{
+int largest;
+
+if (a >= b && a >= c)
+{
+largest = a;
+}
+else if (b >= a && b >= c)
+{
+largest = b;
+}
+else
+{
+largest = c;
+}
+
+return (largest);
+}
