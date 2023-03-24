ed295011db8bda3c9cedc66669321e8416736a36
DCCXVII <elyass.hafdi@gmail.com>
2023-03-23 17:42:44 -0700

    alx-low

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -1,8 +1,11 @@
+#include "main.h"
+
 /**
  * largest_number 
  * @a: first integer
  * @b: second integer
  * @c: third integer
+ * Return: largest number
  */
 
 int largest_number(int a, int b, int c)
