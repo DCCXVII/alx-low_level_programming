e72577f09019cac10e2f0ed9337cbecb593011e2
DCCXVII <elyass.hafdi@gmail.com>
2023-03-31 06:40:26 -0700

    hello world 0-strcat

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -4,6 +4,7 @@
  * _strcat - concatenates two strings
  * @dest: pointer to destination string
  * @src: pointer to source string
+ * Return: pointer to destination string
  */
 char *_strcat(char *dest, char *src)
 {
@@ -15,7 +16,7 @@ while (*dest != '\0')
 dest++;
 }
 
-/* Append the source string to the destination string */
+
 while (*src != '\0')
 {
 *dest = *src;
@@ -23,7 +24,6 @@ dest++;
 src++;
 }
 
-/* Add the terminating null byte */
 *dest = '\0';
 
 return dest_start;
