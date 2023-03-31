6d903c8d66d5c9cced8768554f097070dc5c6bb5
DCCXVII <elyass.hafdi@gmail.com>
2023-03-31 06:21:10 -0700

    alx-system-low

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * _strcat - concatenates two strings
+ * @dest: pointer to destination string
+ * @src: pointer to source string
+ *
+ * Return: pointer to destination string
+ */
+char *_strcat(char *dest, char *src)
+{
+char *dest_start = dest;
+
+/* Find the end of the destination string */
+while (*dest != '\0')
+{
+dest++;
+}
+
+/* Append the source string to the destination string */
+while (*src != '\0')
+{
+*dest = *src;
+dest++;
+src++;
+}
+
+/* Add the terminating null byte */
+*dest = '\0';
+
+return dest_start;
+}
