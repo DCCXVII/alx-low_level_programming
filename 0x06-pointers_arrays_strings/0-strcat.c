6bd61b906f9a48ff7d8f366f2b38906c1a9f5d6f
DCCXVII <elyass.hafdi@gmail.com>
2023-03-31 06:23:00 -0700

    alx-system

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -4,8 +4,6 @@
  * _strcat - concatenates two strings
  * @dest: pointer to destination string
  * @src: pointer to source string
- *
- * Return: pointer to destination string
  */
 char *_strcat(char *dest, char *src)
 {
