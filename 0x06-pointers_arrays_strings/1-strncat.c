2ceb49998f7418eb0afecc5f36ad0f958b755963
DCCXVII <elyass.hafdi@gmail.com>
2023-03-31 19:02:38 -0700

    alx-system_low_level

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,25 +1,28 @@
 #include "main.h"
 
 /**
- *_strncat - comcatenates 2 strings
- * @dest: char
- * @src: char
- * @n:int
- * Return: char
+ * _strncat - Concatenates two strings
+ * @dest: The destination string
+ * @src: The source string
+ * @n: The maximum number of bytes to use from src
+ * Return: A pointer to the resulting string dest
  */
+
 char *_strncat(char *dest, char *src, int n)
 {
-	int i,j;
-	
-	for(i=0;dest[i]!='\0';i++)
-	{
-		continue;
-	}
-	for(j=0;src[j]!='\0' && j<n;j++)
-	{
-		dest[i + j]=src[j];
-	}
+int i, j;
 
-	dest[i + j] = '\0';
-	return (dest);
+for (i = 0; dest[i] != '\0'; i++)
+{
+continue;
+}
+
+for (j = 0; src[j] != '\0' && j < n; j++)
+{
+dest[i + j] = src[j];
+}
+
+dest[i + j] = '\0';
+
+return (dest);
 }
