706c209602b16e7dc7ae7f1101f2b120b3f9a832
DCCXVII <elyass.hafdi@gmail.com>
2023-04-01 14:21:24 -0700

    alx-low-level

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -7,28 +7,28 @@
  */
 int main(void)
 {
-    int i, j, k;
+int i, j, k;
 
-    for (i = 0; i < 100; i++)
-    {
-        j = i % 10;
-        k = i / 10;
+for (i = 0; i < 100; i++)
+{
+j = i % 10;
+k = i / 10;
 
-        if (k < j)
-        {
-            putchar(k + '0');
-            putchar(j + '0');
+if (k < j)
+{
+putchar(k + '0');
+putchar(j + '0');
 
-            if (i < 89)
-            {
-                putchar(',');
-                putchar(' ');
-            }
-        }
-    }
+if (i < 89)
+{
+putchar(',');
+putchar(' ');
+}
+}
+}
 
-    putchar('\n');
+putchar('\n');
 
-    return (0);
+return (0);
 }
 
