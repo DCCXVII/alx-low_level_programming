a473d8cfd51117c0ebd043172b100e3db70e995d
DCCXVII <elyass.hafdi@gmail.com>
2023-04-01 14:11:29 -0700

    alx-low_programming

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,20 +1,26 @@
 #include <stdio.h>
 
+
+/**
+ * main - Prints all possible combinations of two digits
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 int i, j, k;
 
 for (i = 0; i < 100; i++)
 {
-j = i % 10; /* Get the second digit */
-k = i / 10; /* Get the first digit */
+j = i % 10; 
+k = i / 10; 
 
-if (k < j) /* Print only if the first digit is less than the second */
+if (k < j) 
 {
-putchar(k + '0'); /* Convert the digits to characters */
+putchar(k + '0'); 
 putchar(j + '0');
 
-if (i < 89) /* Print the comma and space only if it is not the last combination */
+if (i < 89)
 {
 putchar(','); 
 putchar(' ');
