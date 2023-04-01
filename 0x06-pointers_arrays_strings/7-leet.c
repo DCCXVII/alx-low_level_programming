4baf3557afec74c668cf6a43af29bed935adfda4
DCCXVII <elyass.hafdi@gmail.com>
2023-03-31 20:05:06 -0700

    alx-programming

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -3,26 +3,25 @@
 /**
  * leet - encodes a string into 1337
  * @str: the string to encode
- *
  * Return: the encoded string
  */
 char *leet(char *str)
 {
-    int i, j;
-    char b[] = "4433007711";
-    char a[] = "aeotlAEOTL";
+int i, j;
+char b[] = "4433007711";
+char a[] = "aeotlAEOTL";
 
-    for (i = 0; str[i]; i++)
-    {
-        for (j = 0; a[j]; j++)
-        {
-            if (a[j] == str[i])
-            {
-                str[i] = b[j];
-                break;
-            }
-        }
-    }
+for (i = 0; str[i]; i++)
+{
+for (j = 0; a[j]; j++)
+{
+if (a[j] == str[i])
+{
+str[i] = b[j];
 
-    return (str);
+}
+}
+}
+
+return (str);
 }
