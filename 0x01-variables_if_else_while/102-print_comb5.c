3a6af89043eb0a3b95169552d48ffc1b59e0bdf2
DCCXVII <elyass.hafdi@gmail.com>
2023-04-01 14:37:18 -0700

    alx-system

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,43 +1,45 @@
 #include <stdio.h>
 
-
 /**
- * main - prints all possible different combinations of three digits
+ * main - prints all possible combinations of two two-digit numbers
  *
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-int i, j, k, l;
+    int i, j, k, l;
 
-for (i = 0; i <= 9; i++)
-{
-for (j = 0; j <= 9; j++)
-{
-for (k = i; k <= 9; k++)
-{
-if (k == i) l = j + 1;
+    for (i = 0; i <= 9; i++)
+    {
+        for (j = 0; j <= 9; j++)
+        {
+            for (k = i; k <= 9; k++)
+            {
+                if (k == i)
+                    l = j + 1;
+                else
+                    l = 0;
 
-else l = 0;
-for (; l <= 9; l++)
-{
-putchar(i + '0');
-putchar(j + '0');
-putchar(' ');
-putchar(k + '0');
-putchar(l + '0');
+                for (; l <= 9; l++)
+                {
+                    putchar(i + '0');
+                    putchar(j + '0');
+                    putchar(' ');
+                    putchar(k + '0');
+                    putchar(l + '0');
 
-if (i != 9 || j != 8 || k != 9 || l != 9)
-{
-putchar(',');
-putchar(' ');
-}
-}
-}
-}
+                    if (i != 9 || j != 8 || k != 9 || l != 9)
+                    {
+                        putchar(',');
+                        putchar(' ');
+                    }
+                }
+            }
+        }
+    }
+
+    putchar('\n');
+
+    return (0);
 }
 
-putchar('\n');
-
-return (0);
-}
