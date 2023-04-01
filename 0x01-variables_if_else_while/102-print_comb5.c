66ed5034d0f2de5866dcd3362b93ed907706471d
DCCXVII <elyass.hafdi@gmail.com>
2023-04-01 14:33:43 -0700

    alx-system

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -17,8 +17,8 @@ for (j = 0; j <= 9; j++)
 for (k = i; k <= 9; k++)
 {
 if (k == i) l = j + 1;
-else l = 0;
 
+else l = 0;
 for (; l <= 9; l++)
 {
 putchar(i + '0');
@@ -39,5 +39,5 @@ putchar(' ');
 
 putchar('\n');
 
-return 0;
+return (0);
 }
