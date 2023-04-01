2687b6c898cca89c03cf65a758eac23417b8224a
DCCXVII <elyass.hafdi@gmail.com>
2023-03-31 18:42:38 -0700

    alx-low_programming

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -6,22 +6,21 @@
  * @src: The source string
  * Return: Pointer to the resulting string dest
  */
-
 char *_strcat(char *dest, char *src)
 {
-    char *s = dest;
+char *s = dest;
 
-    while (*dest != '\0')
-    {
-        dest++;
-    }
+while (*dest != '\0')
+{
+dest++;
+}
 
-    while (*src != '\0')
-    {
-        *dest = *src;
-        dest++;
-        src++;
-    }
-    *dest = '\0';
-    return (s);
+while (*src != '\0')
+{
+*dest = *src;
+dest++;
+src++;
+}
+*dest = '\0';
+return (s);
 }
