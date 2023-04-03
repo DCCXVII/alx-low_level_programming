d888b31d8b0f826e10d0310b2d33d3720c4272a2
DCCXVII <elyass.hafdi@gmail.com>
2023-04-03 13:51:06 -0700

    alx-low_programming

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -1,10 +1,10 @@
 #include "main.h"
 
 /**
- * _memcpy - Copies `n` bytes from memory area
- * @dest: Pointer to the destination memory area
- * @src: Pointer to the source memory area
- * @n: Number of bytes to copy
+ * _memcpy - Copies `n` bytes from memory area.
+ * @dest: Pointer to the destination memory area.
+ * @src: Pointer to the source memory area.
+ * @n: Number of bytes to copy.
  */
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
@@ -15,5 +15,5 @@ for (i = 0; i < n; i++)
 dest[i] = src[i];
 }
 
-return dest;
+return (dest);
 }
