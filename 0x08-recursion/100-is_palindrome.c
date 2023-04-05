71524567173cbd269a10de8563a7221ef40d693b
DCCXVII <elyass.hafdi@gmail.com>
2023-04-05 16:49:10 -0700

    alx_low_programming

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -1,36 +1,33 @@
 #include "main.h"
 
 /**
- * _strlen - Returns the length of a string
+ * _strlen_recursion - Returns the length of a string
  * @s: The string to get the length of
  *
  * Return: The length of the string
  */
-int _strlen(char *s)
+int _strlen_recursion(char *s)
 {
 if (*s == '\0')
 return (0);
-return (_strlen(s + 1) + 1);
+return (1 + _strlen_recursion(s + 1));
 }
 
 /**
  * is_palindrome - Checks if a string is a palindrome
  * @s: The string to check
  *
- * Return: 1 if the string is a palindrome, 0 otherwise
+ * Return: 1 if s is a palindrome, 0 otherwise
  */
 int is_palindrome(char *s)
 {
-int len;
+int len = _strlen_recursion(s);
 
-len = _strlen(s);
 if (len <= 1)
 return (1);
 
-if (*s != *(s + len - 1))
+if (*s == *(s + len - 1))
+return (is_palindrome(s + 1) && is_palindrome(s + len - 2));
+
 return (0);
-
-*(s + len - 1) = '\0';
-
-return (is_palindrome(s + 1));
 }
--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -1,35 +1,27 @@
 #include "main.h"
 
 /**
- * wildcmp - Compares two strings and returns 1 if they are identical, otherwise
- * returns 0. The second string can contain the wildcard character '*'.
+ * wildcmp - compares two strings and returns
+ * @s1: the first string
+ * @s2: the second string, which can contain the special character *
  *
- * @s1: First string
- * @s2: Second string with the wildcard character '*'
- *
- * Return: 1 if the strings are identical, otherwise 0.
+ * Return: 1 if the strings can be considered identical, 0 otherwise
  */
 int wildcmp(char *s1, char *s2)
 {
-if (*s2 == '*')
-{
-if (*(s2 + 1) == '*')
-return (wildcmp(s1, s2 + 1));
-
-while (*s1 != '\0')
-{
-if (wildcmp(s1, s2 + 1))
+if (*s1 == '\0' && *s2 == '\0') /* both strings are empty */
 return (1);
-s1++;
-}
-return (*s1 == '\0' && *s2 == '\0');
+
+if (*s2 == '*') /* s2 begins with a wildcard */
+{
+if (*s1 == '\0') /* s1 is empty, check if the rest of s2 is also a wildcard */
+return (wildcmp(s1, s2 + 1));
+else /* try to match the rest of s2 with every possible suffix of s1 */
+return (wildcmp(s1, s2 + 1) || wildcmp(s1 + 1, s2));
 }
 
-if (*s1 == '\0' || *s2 == '\0')
-return (*s1 == *s2);
-
-if (*s1 == *s2)
+if (*s1 == *s2) /* the first character matches */
 return (wildcmp(s1 + 1, s2 + 1));
 
-return (0);
+return (0); /* the first character doesn't match */
 }
