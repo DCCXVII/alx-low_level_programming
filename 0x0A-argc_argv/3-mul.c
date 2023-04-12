daf992547fcc4b3d6762d662a21fe60f1a7aa12f
DCCXVII <elyass.hafdi@gmail.com>
2023-04-12 16:51:53 -0700

    alx-low_level

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -2,15 +2,15 @@
 #include <stdlib.h>
 
 /**
- * main - multiplies two numbers.
- * @argc: number of arguments passed to the program.
- * @argv: array of strings containing the arguments passed to the program.
+ * main - Entry point
+ * @argc: Number of command line arguments
+ * @argv: Array containing the command line arguments
  *
- * Return: Always 0.
+ * Return: 0 if success, 1 if error
  */
-int main(int argc, char **argv)
+int main(int argc, char *argv[])
 {
-int num1, num2, result;
+int a, b, result;
 
 if (argc != 3)
 {
@@ -18,12 +18,10 @@ printf("Error\n");
 return (1);
 }
 
-num1 = atoi(argv[1]);
-num2 = atoi(argv[2]);
-
-result = num1 * num2;
+a = atoi(argv[1]);
+b = atoi(argv[2]);
+result = a * b;
 
 printf("%d\n", result);
-
 return (0);
 }
