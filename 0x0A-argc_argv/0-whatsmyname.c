bbb7dd7b017275d43cd6e0654d4d1bc247b8b4b6
DCCXVII <elyass.hafdi@gmail.com>
2023-04-12 16:34:09 -0700

    alx_low_programming

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,13 +1,5 @@
 #include <stdio.h>
-#include "main.h"
 
-/**
- * main - Entry point
- * @argc: The number of command-line arguments
- * @argv: An array containing the command-line arguments
- *
- * Return: 0 on success
- */
 int main(int argc, char *argv[])
 {
 printf("%s\n", argv[0]);
