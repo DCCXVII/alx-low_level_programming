0eca4bd116c9eb3523e2a15d507448eb8a618fc4
DCCXVII <elyass.hafdi@gmail.com>
2023-04-17 22:10:13 -0700

    alx_low_programming

---
+++ 0x0D-preprocessor/0-object_like_macro.h
@@ -0,0 +1,9 @@
+#ifndef 0_OBJECT_LIKE_MACRO_H
+#define 0_OBJECT_LIKE_MACRO_H
+/*
+ * Description this is header defines a macro named SIZE
+ */
+
+#define SIZE 1024
+
+#endif /* 0_OBJECT_LIKE_MACRO_H */
