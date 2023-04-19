53427c58f9f87522a473cdb00a807ea8a96d697f
DCCXVII <elyass.hafdi@gmail.com>
2023-04-19 14:35:16 -0700

    alx-low-level_programming

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -1,10 +1,12 @@
+#include <stddef.h>
 #include "function_pointers.h"
 
 /**
- * array_iterator
- * @array: pointer to the array
+ * array_iterator - executes a function on each element of an array
+ * @array: the array to iterate over
  * @size: size of the array
- * @action: pointer to the function to be executed
+ * @action: pointer to the function to use
+ *
  * Return: Nothing.
  */
 void array_iterator(int *array, size_t size, void (*action)(int))
@@ -14,8 +16,6 @@ size_t i;
 if (array && action)
 {
 for (i = 0; i < size; i++)
-{
 action(array[i]);
-}
 }
 }
