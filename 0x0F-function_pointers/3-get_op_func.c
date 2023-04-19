6c2b31c27c5f311154fae42adc0ca93174a2336f
DCCXVII <elyass.hafdi@gmail.com>
2023-04-19 14:48:47 -0700

    alx-low-level_programming

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -3,29 +3,27 @@
 /**
  * get_op_func - selects the correct function to perform the operation
  * @s: operator passed as argument to the program
- *
- * Return: pointer to the function that corresponds to the operator given
- * as a parameter. NULL if s does not match any of the 5 expected operators
+ * Return : lah oealm
  */
 int (*get_op_func(char *s))(int, int)
 {
-    op_t ops[] = {
-        {"+", op_add},
-        {"-", op_sub},
-        {"*", op_mul},
-        {"/", op_div},
-        {"%", op_mod},
-        {NULL, NULL}
-    };
+op_t ops[] = {
+{"+", op_add},
+{"-", op_sub},
+{"*", op_mul},
+{"/", op_div},
+{"%", op_mod},
+{NULL, NULL}
+};
 
-    int i = 0;
+int i = 0;
 
-    while (ops[i].op)
-    {
-        if (*s == *(ops[i].op) && *(s + 1) == '\0')
-            return (ops[i].f);
-        i++;
-    }
+while (ops[i].op)
+{
+if (*s == *(ops[i].op) && *(s + 1) == '\0')
+return (ops[i].f);
+i++;
+}
 
-    return (NULL);
+return (NULL);
 }
